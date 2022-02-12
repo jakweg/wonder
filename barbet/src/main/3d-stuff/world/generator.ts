65048ca43d6dbfc1e5bd8a61afdfd40e48e47491
jakweg <jakubek.weg@gmail.com>
2022-02-12 23:10:22 +0100

    Add rivers and improve biomes generation

--- barbet/src/main/3d-stuff/world/generator.ts
+++ barbet/src/main/3d-stuff/world/generator.ts
@@ -8,10 +8,10 @@ export interface GeneratorSettings extends WorldSize {
 }
 
 const getBiomeByValue = (value: number): BiomeId => {
-	if (value < -0.5)
+	if (value < -0.4)
 		return BiomeId.Snowy
 
-	if (value < 0.5)
+	if (value < 0.3)
 		return BiomeId.Forest
 
 	return BiomeId.Desert
@@ -23,13 +23,17 @@ export const generateBiomeMap = (settings: GeneratorSettings): Uint8Array => {
 	if (BiomeId.Void !== 0) data.fill(BiomeId.Void)
 
 	const noise = makeNoise2D(settings.biomeSeed)
-	const factor = 0.004
+	const noise2 = makeNoise2D(settings.biomeSeed)
+	const factor = 0.003
+	const factor2 = 0.04
 	let index = 0
 	for (let z = 0; z < sizeZ; z++) {
 		const zFactored = z * factor
+		const zFactored2 = z * factor2
 		for (let x = 0; x < sizeX; x++) {
 			const value = noise(x * factor, zFactored)
-			data[index++] = getBiomeByValue(value)
+			const value2 = noise2(x * factor2, zFactored2)
+			data[index++] = getBiomeByValue((value * 9 + value2) / 10)
 		}
 	}
 
@@ -48,13 +52,16 @@ export const generateHeightMap = (settings: GeneratorSettings): Uint8Array => {
 	const noise2 = makeNoise2D(settings.heightSeed)
 	const noise3 = makeNoise2D(settings.heightSeed)
 	const noise4 = makeNoise2D(settings.heightSeed)
+	const noise5 = makeNoise2D(settings.heightSeed)
 	const factor = 0.025
 	const factor2 = 0.005
 	const factor3 = 0.002
 	const factor4 = 0.001
+	const factor5 = 0.01
 
-	const foo2 = (x: number): number => Math.atan(20 * (x - 0.5)) / Math.PI + 0.5
+	const foo2 = (x: number): number => Math.atan(80 * (x - 0.5)) / Math.PI + 0.5
 	const foo3 = (x: number): number => Math.abs(Math.atan(128 * x) / (Math.PI / 2)) ** 4
+	const foo4 = (x: number): number => (Math.abs(1 / (1 + Math.E ** (-10 * (x + 1))) - 0.5) * 2) ** 5
 
 	let index = 0
 	for (let z = 0; z < sizeZ; z++) {
@@ -62,12 +69,16 @@ export const generateHeightMap = (settings: GeneratorSettings): Uint8Array => {
 		const zFactored2 = z * factor2
 		const zFactored3 = z * factor3
 		const zFactored4 = z * factor4
+		const zFactored5 = z * factor5
 		for (let x = 0; x < sizeX; x++) {
 			const value = (noise(x * factor, zFactored) + 1) * 0.5 ** 2
 			const value2 = (noise2(x * factor2, zFactored2) + 1) * 0.5
 			const value3 = foo3(noise3(x * factor3, zFactored3))
 			const value4 = foo3(noise4(x * factor4, zFactored4))
-			data[index] = ((foo2(value2) * 5 + value * 2) / 7 * 0.7 + 0.3) * value3 * value4 * sizeY | 0
+			const value5 = foo4(noise5(x * factor5, zFactored5))
+
+			const height = ((foo2(value2) * 5 + value * 2) / 7 * 0.7 + 0.3) * value3 * value4 * value5
+			data[index] = height * sizeY | 0
 			index++
 		}
 	}
