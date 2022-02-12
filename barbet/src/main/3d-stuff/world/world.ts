4384c84f84ecb6f4525193227550c8dac1a41917
jakweg <jakubek.weg@gmail.com>
2022-02-12 21:18:46 +0100

    Refactor: new world generator, blocks and biomes support

--- barbet/src/main/3d-stuff/world/world.ts
+++ barbet/src/main/3d-stuff/world/world.ts
@@ -1,4 +1,4 @@
-import { AIR, BlockType } from './block'
+import { AIR_ID, BlockId } from './block'
 
 export interface WorldSize {
 	readonly sizeX: number,
@@ -8,6 +8,7 @@ export interface WorldSize {
 
 export interface ComputedWorldSize extends WorldSize {
 	readonly totalBlocks: number
+	readonly blocksPerY: number
 }
 
 export class World {
@@ -20,16 +21,27 @@ export class World {
 	public static createEmpty(sizeX: number,
 	                          sizeY: number,
 	                          sizeZ: number,
-	                          fillWith: BlockType = AIR): World {
-		const totalBlocks = sizeX * sizeY * sizeZ
+	                          fillWith: BlockId = AIR_ID): World {
+		const blocksPerY = sizeX * sizeZ
+		const totalBlocks = sizeY * blocksPerY
 
-		const size = {sizeX, sizeY, sizeZ, totalBlocks}
+		const size = {sizeX, sizeY, sizeZ, totalBlocks, blocksPerY}
 		const blockData = new Uint8Array(totalBlocks)
 
 		// by default all content is 0 so no need to reset that
-		if (fillWith.numericId !== 0)
-			blockData.fill(fillWith.numericId)
+		if (fillWith !== 0)
+			blockData.fill(fillWith)
 
 		return new World(size, blockData)
+	}
+
+	public setBlock(x: number, y: number, z: number, blockId: BlockId) {
+		this.validateCoords(x, y, z)
+		this.rawBlockData[y * this.size.blocksPerY + x * this.size.sizeX + z] = blockId
+	}
+
+	private validateCoords(x: number, y: number, z: number) {
+		if (x < 0 || x >= this.size.sizeX || y < 0 || y >= this.size.sizeY || z < 0 || z >= this.size.sizeZ)
+			throw new Error(`Invalid coords ${x} ${y} ${z}`)
 	}
 }
--- barbet/src/main/main.ts
+++ barbet/src/main/main.ts
@@ -1,7 +1,9 @@
 import { MainRenderer } from './3d-stuff/main-renderer'
 import { RenderContext } from './3d-stuff/renderable/render-context'
 import { createNewTerrainRenderable } from './3d-stuff/renderable/terrain'
-import { allBlocks, BlockId } from './3d-stuff/world/block'
+import { allBiomes } from './3d-stuff/world/biome'
+import { BlockId } from './3d-stuff/world/block'
+import { generateBiomeMap, generateHeightMap } from './3d-stuff/world/generator'
 import { World } from './3d-stuff/world/world'
 import { Camera } from './camera'
 import KEYBOARD from './keyboard-controller'
@@ -13,9 +15,35 @@ canvas.height = 720
 
 const renderer = MainRenderer.fromHTMLCanvas(canvas)
 const camera = Camera.newPerspective(90, 1280 / 720)
-camera.moveCamera(255, 50, 255)
+camera.moveCamera(25, 80, 180)
 
-const world = World.createEmpty(10, 10, 10, allBlocks[BlockId.Stone])
+const world = World.createEmpty(1000, 30, 1000, BlockId.Air)
+const settings = {...world.size, biomeSeed: 123, heightSeed: 1234}
+const biomeMap = generateBiomeMap(settings)
+const heightMap = generateHeightMap(settings)
+
+let index = 0
+for (let z = 0; z < settings.sizeZ; z++) {
+	for (let x = 0; x < settings.sizeX; x++) {
+		const biomeValue = allBiomes[biomeMap[index]!]!
+		const yHere = heightMap[index]!
+		world.setBlock(x, yHere, z, biomeValue.surfaceMaterialId)
+		const underSurfaceMaterialId = biomeValue.underSurfaceMaterialId
+		for (let y = 0; y < yHere; ++y)
+			world.setBlock(x, y, z, underSurfaceMaterialId)
+
+		if (yHere < 4) {
+			const waterSurfaceMaterialId = biomeValue.waterSurfaceMaterialId
+			const upperWaterLimit = 3 + (waterSurfaceMaterialId === BlockId.Water ? 0 : 1)
+			for (let y = 0; y <= upperWaterLimit; ++y)
+				world.setBlock(x, y, z, waterSurfaceMaterialId)
+		}
+		index++
+	}
+}
+for (let i = 0; i < settings.sizeY; i++) {
+	world.setBlock(100, i, 100, BlockId.Stone)
+}
 const terrain = createNewTerrainRenderable(renderer, world)
 
 const sunPosition = vec3.fromValues(-300, 2500, -1000)
