f0592780f4636233beca36d7a9df6137ab88bb9c
jakweg <jakubek.weg@gmail.com>
2022-03-07 23:27:22 +0100

    Implement procedurally generated resource shape

--- barbet/src/main/3d-stuff/game-state/resources/stone.ts
+++ barbet/src/main/3d-stuff/game-state/resources/stone.ts
@@ -1,59 +1,44 @@
+import { calculateNormals } from '../../shader/common'
 import { ItemType } from '../../world/item'
 import { SurfaceResourceType } from '../../world/surface-resource'
 
-const rootVertexes = new Float32Array([
-	0, 0.7, 0, 0, 0, 0,
-	-0.5, 0, -0.5, 0, 0, 0,
-	-0.5, 0, 0.5, 0, 0, 0,
-	0.5, 0, 0.5, 0, 0, 0,
-	0.5, 0, -0.5, 0, 0, 0,
-])
+const createVertexesAndElements = (size: number) => {
+	if (size !== (size | 0) || size < 3 || size > 100)
+		throw new Error('Invalid size')
 
-const elements: Readonly<Uint8Array> = new Uint8Array([
-	2, 0, 1,
-	3, 0, 2,
-	4, 0, 3,
-	1, 0, 4,
-])
+	const vertexData = new Float32Array((size + 1) * 6)
+	const elementsData = new Uint8Array(size * 3)
+
+	vertexData[0] = 0.5
+	vertexData[1] = 0.75
+	vertexData[2] = 0.5
+
+	const initialAngle = Math.PI / 4
+	const r = .55
+	for (let i = 1; i <= size; i++) {
+		const x = Math.cos(i / size * 2 * Math.PI + initialAngle) * r
+		const y = Math.sin(i / size * 2 * Math.PI + initialAngle) * r
+
+		vertexData[i * 6] = x + 0.5
+		vertexData[i * 6 + 1] = 0
+		vertexData[i * 6 + 2] = y + 0.5
+
+		elementsData[(i - 1) * 3] = i
+		elementsData[(i - 1) * 3 + 1] = 0
+		elementsData[(i - 1) * 3 + 2] = i % size + 1
+	}
+
+	return {vertexData, elementsData}
+}
+
+const {vertexData, elementsData} = createVertexesAndElements(6)
 
 let initialized = false
 const initData = () => {
 	if (initialized) return
 	initialized = true
-	for (let i = 0, l = elements.length; i < l;) {
-		const a = elements[i++]!
-		const b = elements[i++]!
-		const c = elements[i++]!
-
-		const VERTEX_SIZE = 6
-
-		const aIndex = a * VERTEX_SIZE
-		const ax = rootVertexes[aIndex]!
-		const ay = rootVertexes[aIndex + 1]!
-		const az = rootVertexes[aIndex + 2]!
-
-		const bIndex = b * VERTEX_SIZE
-		const bx = rootVertexes[bIndex]!
-		const by = rootVertexes[bIndex + 1]!
-		const bz = rootVertexes[bIndex + 2]!
-
-		const cIndex = c * VERTEX_SIZE
-		// const cx = rootVertexes[cIndex]!
-		// const cy = rootVertexes[cIndex + 1]!
-		// const cz = rootVertexes[cIndex + 2]!
-
-		const nx = ay * bz - az * by
-		const ny = az * bx - ax * bz
-		const nz = ax * by - ay * bx
-
-		rootVertexes[cIndex + 3] = nx
-		rootVertexes[cIndex + 4] = ny
-		rootVertexes[cIndex + 5] = nz
-	}
-	for (let i = 0, l = rootVertexes.length; i < l; i += 6) {
-		rootVertexes[i] = rootVertexes[i]! + 0.5
-		rootVertexes[i + 2] = rootVertexes[i + 2]! + 0.5
-	}
+	calculateNormals(elementsData, vertexData, 6, 3)
+	console.log({vertexData, elementsData})
 }
 
 export default {
@@ -61,24 +46,24 @@ export default {
 	gatheredItem: ItemType.Box,
 
 	appendToMesh(x: number, y: number, z: number,
-	             vertexData: number[],
-	             elementsData: number[]): void {
+	             vertexDataToAppend: number[],
+	             elementsDataToAppend: number[]): void {
 		initData()
 
-		const vertexCountBeforeAdd = vertexData.length / 4 | 0
-		const vertexes = rootVertexes
+		const vertexCountBeforeAdd = vertexDataToAppend.length / 6 | 0
+		const vertexes = vertexData
 		for (let i = 0, s = vertexes.length; i < s;) {
 			const vx = vertexes[i++]! + x
 			const vy = vertexes[i++]! + y
 			const vz = vertexes[i++]! + z
-			vertexData.push(vx, vy, vz,
+			vertexDataToAppend.push(vx, vy, vz,
 				vertexes[i++]!,
 				vertexes[i++]!,
 				vertexes[i++]!)
 		}
 
-		for (const index of elements) {
-			elementsData.push(index + vertexCountBeforeAdd)
+		for (const index of elementsData) {
+			elementsDataToAppend.push(index + vertexCountBeforeAdd)
 		}
 	},
 }
