e972bc75c36f1e915dc6b4d675c75ab2451271b7
jakweg <jakubek.weg@gmail.com>
2022-04-24 21:57:23 +0200

    Create spawn-unit sync action

---
+++ barbet/src/main/game-state/sync-operations/spawn-unit.ts
@@ -0,0 +1,44 @@
+import { UnitColorPaletteId } from '../../3d-stuff/renderable/unit/unit-color'
+import * as idle from '../activities/idle'
+import { DataOffsetPositions, EntityTrait } from '../entities/traits'
+import { GameState } from '../game-state'
+
+interface Props {
+	game: GameState
+	x: number
+	z: number
+	color: UnitColorPaletteId
+}
+
+const enum ErrorReason {
+	CoordinatesOutOfBounds,
+	InvalidHeightOfCoordinates,
+}
+
+type Result = { success: false, reason: ErrorReason } | { success: true, unitId: number }
+
+const unitTraits = EntityTrait.Position | EntityTrait.Drawable | EntityTrait.ItemHoldable | EntityTrait.WithActivity | EntityTrait.Interruptible
+
+export default (props: Props): Result => {
+	const x = props.x
+	const z = props.z
+
+	if (x < 0 || x >= props.game.world.size.sizeX || x !== (x | 0)
+		|| z < 0 || z >= props.game.world.size.sizeZ || z !== (z | 0))
+		return {success: false, reason: ErrorReason.CoordinatesOutOfBounds}
+
+	const y = props.game.world.getHighestBlockHeight(x, z)
+	if (y <= 0)
+		return {success: false, reason: ErrorReason.InvalidHeightOfCoordinates}
+
+	const entities = props.game.entities
+
+	const unit = entities.createEntity(unitTraits)
+	entities.positions.rawData[unit.position + DataOffsetPositions.PositionX] = x
+	entities.positions.rawData[unit.position + DataOffsetPositions.PositionY] = y + 1
+	entities.positions.rawData[unit.position + DataOffsetPositions.PositionZ] = z
+
+	idle.setup(props.game, unit)
+
+	return {success: true, unitId: unit.thisId}
+}
--- barbet/src/main/worker/example-world-creator.ts
+++ barbet/src/main/worker/example-world-creator.ts
@@ -1,33 +1,37 @@
-import * as activityIdle from '../game-state/activities/idle'
-import { DataOffsetPositions, EntityTrait } from '../game-state/entities/traits'
+import { UnitColorPaletteId } from '../3d-stuff/renderable/unit/unit-color'
 import { GameState } from '../game-state/game-state'
+import { ItemType } from '../game-state/items'
 import fillTerrain from '../game-state/sync-operations/fill-terrain'
+import spawnUnit from '../game-state/sync-operations/spawn-unit'
 import { BlockId } from '../game-state/world/block'
 
-export function fillEmptyWorldWithDefaultData(gameState: GameState) {
+export function fillEmptyWorldWithDefaultData(game: GameState) {
 
-	const {world} = gameState
+	const {world, groundItems} = game
 
 	fillTerrain({
-		game: gameState, fillWith: BlockId.Grass,
+		game, fillWith: BlockId.Grass,
 		x: 3, sx: world.size.sizeX - 6,
 		y: 0, sy: 2,
 		z: 3, sz: world.size.sizeZ - 6,
 	})
 
 	fillTerrain({
-		game: gameState, fillWith: BlockId.Sand, replace: BlockId.Air,
+		game, fillWith: BlockId.Sand, replace: BlockId.Air,
 		x: 2, sx: world.size.sizeX - 4,
 		y: 0, sy: 2,
 		z: 2, sz: world.size.sizeZ - 4,
 	})
 
 	fillTerrain({
-		game: gameState, fillWith: BlockId.Water, replace: BlockId.Air,
+		game, fillWith: BlockId.Water, replace: BlockId.Air,
 		x: 0, sx: world.size.sizeX,
 		y: 0, sy: 1,
 		z: 0, sz: world.size.sizeZ,
 	})
+
+	spawnUnit({game, x: 7, z: 8, color: UnitColorPaletteId.DarkBlue})
+
 
 	groundItems.setItem(17, 14, ItemType.Box)
 	groundItems.setItem(16, 14, ItemType.Box)
@@ -35,16 +39,5 @@ export function fillEmptyWorldWithDefaultData(gameState: GameState) {
 	groundItems.setItem(14, 14, ItemType.Box)
 	groundItems.setItem(13, 14, ItemType.Box)
 	groundItems.setItem(3, 3, ItemType.Box)
-
-	const spawnUnit = (x: number, y: number, z: number) => {
-		const unitTraits = EntityTrait.Position | EntityTrait.Drawable | EntityTrait.ItemHoldable | EntityTrait.WithActivity | EntityTrait.Interruptible
-		const entity = entities.createEntity(unitTraits)
-		entities.positions.rawData[entity.position + DataOffsetPositions.PositionX] = x
-		entities.positions.rawData[entity.position + DataOffsetPositions.PositionY] = y
-		entities.positions.rawData[entity.position + DataOffsetPositions.PositionZ] = z
-
-		activityIdle.setup(gameState, entity)
-	}
-	spawnUnit(7, 2, 8)
 }
 
