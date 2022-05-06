bd759a81ee4878273c42abe8a6a0c26690fd3691
jakweg <jakubek.weg@gmail.com>
2022-05-06 21:28:21 +0200

    Add fps cap setting

--- barbet/src/main/ui/preferences/index.ts
+++ barbet/src/main/ui/preferences/index.ts
@@ -34,6 +34,7 @@ const Main = (root: HTMLElement) => {
 	BooleanSwitch(main, 'rendering/antialias', (v: boolean) => `Antialiasing: ${v ? 'ON' : 'OFF'}`)
 	BooleanSwitch(main, 'rendering/show-tile-borders', (v: boolean) => `Tile borders: ${v ? 'ON' : 'OFF'}`)
 	BooleanSwitch(main, 'other/pause-on-blur', (v: boolean) => `Auto pause: ${v ? 'ON' : 'OFF'}`)
+	FpsCapSetting(main)
 }
 
 const BooleanSwitch = (main: HTMLElement,
@@ -44,11 +45,66 @@ const BooleanSwitch = (main: HTMLElement,
 }
 
 const Button = (root: HTMLElement,
-                       displayedText: Observable<string>,
-                       onClick: Callback) => {
+                displayedText: Observable<string>,
+                onClick: Callback) => {
 	const container = createElement('button', root, 'setting-button') as HTMLButtonElement
 	container['type'] = 'button'
 	container.addEventListener('click', onClick)
 
 	displayedText(displayedText => container.innerText = displayedText)
+}
+
+const FpsCapSetting = (main: HTMLElement) => {
+	const fpsCap = observeField(CONFIG, 'rendering/fps-cap')
+	const mapTitle = (v: number) => `Max framerate: ${v === 0 ? 'VSYNC' : v}`
+	Range(main, map(fpsCap, mapTitle), [0, 60], 5, fpsCap, (value) => CONFIG.set('rendering/fps-cap', value))
+}
+
+const Range = (root: HTMLElement,
+               displayedText: Observable<string>,
+               range: [number, number],
+               step: number,
+               value: Observable<number>,
+               setValue: (value: number) => void) => {
+	const container = createElement('div', root, 'setting-range') as HTMLDivElement
+	const movingPart = createElement('div', container, 'moving-part') as HTMLDivElement
+	const title = createElement('div', container, 'title') as HTMLDivElement
+
+	const stepsCount = (range[1] - range[0]) / step
+
+	const handleDragMove = (event: PointerEvent) => {
+		let progress = event.offsetX / (event.target as HTMLDivElement).clientWidth
+		if (progress < 0)
+			progress = 0
+		else if (progress > 1)
+			progress = 1
+		progress = Math.round(progress * stepsCount) / stepsCount
+		setValue(Math.round((progress * (range[1] - range[0]) + range[0])))
+	}
+
+	value(value => {
+		const progress = (value - range[0]) / (range[1] - range[0])
+		movingPart['style']['setProperty']('--progress', `${Math.round(progress * 100) | 0}%`)
+	})
+
+	let dragging = false
+	container.addEventListener('pointerdown', event => {
+		dragging = true
+		handleDragMove(event)
+	})
+	container.addEventListener('pointerup', event => {
+		if (!dragging) return
+		dragging = false
+		handleDragMove(event)
+	})
+	container.addEventListener('pointerleave', event => {
+		if (!dragging) return
+		dragging = false
+		handleDragMove(event)
+	})
+	container.addEventListener('pointermove', event => {
+		if (dragging) handleDragMove(event)
+	}, {'passive': true})
+
+	displayedText(displayedText => title.innerText = displayedText)
 }
