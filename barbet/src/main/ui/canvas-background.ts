45d2520e6ac3445b7079c6bb3554cba19e28ba1a
jakweg <jakubek.weg@gmail.com>
2022-05-07 12:43:58 +0200

    Add smooth canvas recreation

--- barbet/src/main/ui/canvas-background.ts
+++ barbet/src/main/ui/canvas-background.ts
@@ -7,8 +7,21 @@ export default (parent: HTMLElement) => {
 	return {
 		recreate(): HTMLCanvasElement {
 			cancelListenersCallback()
-			const newElement = document.createElement('canvas') as HTMLCanvasElement
-			parent['replaceChild'](newElement, element)
+			const newElement = document['createElement']('canvas') as HTMLCanvasElement
+			newElement['width'] = 0
+			newElement['height'] = 0
+			let triesCounter = 0
+			const old = element
+			const intervalId = setInterval(() => {
+				if (newElement['width'] !== 0 || triesCounter++ > 40) {
+					clearInterval(intervalId)
+					old['classList']['add']('fade-out')
+					setTimeout(() => {
+						old['remove']()
+					}, 1500)
+				}
+			}, 50)
+			parent['insertBefore'](newElement, element)
 			element = newElement
 			cancelListenersCallback = bindFrontendVariablesToCanvas(newElement)
 			return newElement
