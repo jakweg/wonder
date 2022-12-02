9c3fb9af2cd922fe7e811260b67d9e8c78b6aa57
Jakub Wegrzyn <jakubek.weg@gmail.com>
2022-12-02 21:40:23 +0100

    Fix hash calculation

--- ginkgo/on-prem-webhook/webhook/main.ts
+++ ginkgo/on-prem-webhook/webhook/main.ts
@@ -1,4 +1,5 @@
-import { exec, spawn } from "child_process";
+import { spawn } from "child_process";
+import { createHmac } from "crypto";
 import { readFileSync } from "fs";
 import { createServer, ServerResponse } from "http";
 
@@ -12,25 +13,16 @@ const MASTER_BRANCH_NAME = envFile.MASTER || "master";
 const GITHUB_SECRET = envFile.GITHUB_SECRET || "";
 
 if (!GITHUB_SECRET) console.error("Missing github secret!");
-console.log({ GITHUB_KEY: GITHUB_SECRET });
 
 const causeUpdate = () => {
   console.log("Trggered update!");
 };
 
-async function computeHash(content: Buffer): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const process = exec(
-      `openssl sha256 -hmac ${GITHUB_SECRET}`,
-      (err, output) => {
-        if (err) reject(err);
-        else resolve(output);
-      }
-    );
-    process.stdin.write(content);
-  });
-}
-const handle = async (
+const computeHash = (content: Buffer): string => {
+  return createHmac("sha256", GITHUB_SECRET).update(content).digest("hex");
+};
+
+const handle = (
   res: ServerResponse,
   githubSignature: string | undefined,
   githubAction: string | undefined,
@@ -39,8 +31,10 @@ const handle = async (
   if (!githubAction) return res.writeHead(400).end();
   if (!githubSignature) return res.writeHead(401).end();
 
-  const computedHash = await computeHash(body);
-  if (githubSignature !== computedHash) return res.writeHead(403).end();
+  const computedHash = computeHash(body);
+
+  if (githubSignature.substring("sha256=".length) !== computedHash)
+    return res.writeHead(403).end();
 
   if (githubAction === "ping") return res.writeHead(200).end();
   if (githubAction !== "push") return res.writeHead(405).end();
@@ -65,7 +59,7 @@ createServer((req, res) => {
 
     postData.push(chunk);
   });
-  req.on("end", async () => {
+  req.on("end", () => {
     try {
       const totalSize = postData.reduce((a, c) => a + c.length, 0);
       if (totalSize > 16 * 1024 * 1024) {
@@ -83,7 +77,7 @@ createServer((req, res) => {
 
       const githubSignature = req.headers["x-hub-signature-256"];
       const event = req.headers["x-github-event"];
-      await handle(
+      handle(
         res,
         Array.isArray(githubSignature) ? githubSignature[0] : githubSignature,
         Array.isArray(event) ? event[0] : event,
@@ -99,51 +93,5 @@ createServer((req, res) => {
     }
   });
 }).listen(port, undefined, () => {
-  console.log("Listening at port " + port);
+  console.log("Webhook is ready");
 });
-
-// import { serve } from 'https://deno.land/std@0.119.0/http/server.ts';
-// import 'https://deno.land/x/dotenv/mod.ts';
-
-// const MASTER_BRANCH_NAME = 'master'
-// const GITHUB_SECRET = new TextDecoder().decode(await Deno.readFile('github-secret.txt'))
-
-// async function causeUpdate() {
-// 	console.info('Requested update, launching compilation');
-
-// }
-
-// async function handler(request: Request): Promise<Response> {
-// 	try {
-// 		const bodyBuffer = await request.arrayBuffer()
-// 		const hash = await computeHash(GITHUB_SECRET, new Uint8Array(bodyBuffer))
-// 		const signatureFromGithub = request.headers.get('X-Hub-Signature-256')?.substring(7)
-// 		if (hash !== signatureFromGithub)
-// 			return new Response('Invalid hash', { status: 403 })
-
-// 		const action = request.headers.get('X-GitHub-Event')
-
-// 		if (action === 'ping')
-// 			return new Response(null, { status: 204 })
-
-// 		if (action !== 'push')
-// 			return new Response('Invalid action', { status: 400 })
-
-// 		const body = JSON.parse(new TextDecoder().decode(new Uint8Array(bodyBuffer)))
-// 		const expectedRef = `refs/heads/${MASTER_BRANCH_NAME}`
-// 		if (body.ref !== expectedRef)
-// 			return new Response(`Ignoring: ref is ${body.ref}, but expected ${expectedRef}`, { status: 200 })
-
-// 		causeUpdate()
-
-// 		return new Response(null, { status: 201 })
-// 	} catch (e) {
-// 		console.error(e)
-// 		return new Response(null, { status: 500 })
-// 	}
-// }
-
-// const PORT = +(Deno.env.get('PORT') ?? '3000') || 3000
-// serve(handler, {
-// 	port: PORT,
-// })
