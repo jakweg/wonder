aa2607084f833e3c0f2f35ef1335b43477ea2044
Jakub Wegrzyn <jakubek.weg@gmail.com>
2022-12-03 21:55:59 +0100

    Remove GCP Continuous deployment pipeline

--- ginkgo/github-webhook/deps.ts
+++
@@ -1,2 +0,0 @@
-import 'https://deno.land/std@0.119.0/http/server.ts'
-import 'https://deno.land/x/dotenv/mod.ts'
--- ginkgo/hosting-updater/deps.ts
+++
@@ -1,3 +0,0 @@
-import 'https://deno.land/std@0.119.0/http/server.ts'
-import 'https://deno.land/x/dotenv/mod.ts'
-import 'https://deno.land/x/esbuild@v0.14.25/mod.js'
